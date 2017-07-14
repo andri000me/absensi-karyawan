bba01f17349cad14d1c52f5e4341d776b4149d43
prakerinrpl2017 <mfthereds99@gmail.com>
2017-07-14 15:02:17 -0800

    file_baru

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Absensi Karyawan</title>
+    <link rel="stylesheet" href="css & Bootstrap/master.css">
+  </head>
+  <body>
+      <header>
+      <nav>
+        <div class="topnav" id="myTopnav">
+  <a href="#">Home</a>
+  <a href="#">Absensi</a>
+  <a href="#">Login</a>
+  <a href="#">Logout</a>
+  <img src="image/idch.png" height="70px" width="250px">
+        </div>
+      </nav>
+      </header>
+
+  <footer>
+      <p>
+      &copy; 2017
+      </p>
+  </footer>
+  </body>
+</html>
---
+++ koneksi.php
@@ -0,0 +1,4 @@
+<?php
+mysql_connect("localhost","root","")or die ("koneksi ke server gagal");
+mysql_select_db("absensi_karyawan") or die ("database tidak ditemukan");
+?>
