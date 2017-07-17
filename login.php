d02446cd671bf7516eba9fb73c23caa36fa364e8
prakerinrpl2017 <mfthereds99@gmail.com>
2017-07-17 09:58:57 +0700

    Add files via upload
    penambahan form login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="font-awesome.css">
+</head>
+<body>
+<div class="container">
+<img src="19nNcx_8_400x400.jpg" style="border-radius: 15px;">
+	<form>
+		<div class="input-form">
+			<input type="text" name="username" placeholder="Enter Username">
+		</div>
+		<div class="input-form">
+			<input type="password" name="password" placeholder="Enter Password">
+		</div>
+		<select class="level" style="height: 30px; width: 150px; margin-top: 10px; margin-bottom: 20px; border-radius: 5px;">
+		<option value="lev1">Level 1</option>
+		<option value="lev2">Level 2</option>
+		<option value="lev3">Level 3</option>
+		</select><br>
+		<input type="submit" name="submit" value="LOGIN" class="btn-login"><br>
+		<a><p>Forget Password?</p></a>
+	</form>
+</div>
+
+</body>
+</html>
